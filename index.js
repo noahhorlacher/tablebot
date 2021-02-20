15dace91a5a0c4a749bba5c55be37ce59b06c682
Noah Horlacher <kuromarumusic@gmail.com>
2021-02-20 15:48:40 +0100

    Cleanup and fetching files

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 // Import discord.js and create the client
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const mime = require('mime-types')
 
 // Import dotenv for getting the commandtoken
 require('dotenv').config()
 
-// Helper function for getting the last array element
+// Helper functions
 if (!Array.prototype.last) {
 	Array.prototype.last = function () {
 		return this[this.length - 1]
 	}
+}
+function msToTime(s) {
+	var ms = s % 1000
+	s = (s - ms) / 1000
+	var secs = s % 60
+	s = (s - secs) / 60
+	var mins = s % 60
+	var hrs = (s - mins) / 60
+
+	return `\`${hrs}h ${mins}m ${secs}s\``
 }
 
 // Define bot filetypes
@@ -20,16 +31,31 @@ let bot = {
 	}
 }
 
-// return first valid image or false if none
-let msgImage = (msg) => {
-	return msg.attachments.find((e) =>
-		bot.filetypes.image.includes(e.name.split('.').last())
+// return msg file urls or false
+let msgUrls = (s) => {
+	return (
+		s.match(
+			/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
+		) || false
 	)
 }
 
+let urlFileType = (s) => {
+	let l = s.split('/').pop()
+	return l == l.split('.') ? undefined : '.' + l.split('.').pop()
+}
+
+// return msg image or false
+let msgImage = (msg) => {
+	console.log('msgurls:', msgUrls(msg.cleanContent))
+	return msgUrls(msg.cleanContent).find((e) =>
+		bot.filetypes.sheet.includes(urlFileType(e))
+	)
+}
+// return msg sheet or false
 let msgSheet = (msg) => {
 	return msg.attachments.find((e) =>
-		bot.filetypes.sheet.includes(e.name.split('.').last())
+		bot.filetypes.sheet.includes('.' + e.name.split('.').last())
 	)
 }
 
@@ -39,9 +65,7 @@ let ctk = cfg.commandtoken
 
 bot.commands = {
 	help: {
-		token: 'help',
 		descr: 'General help',
-		example: `${ctk} help`,
 		action: (msg) => {
 			msg.reply({
 				embed: {
@@ -50,12 +74,12 @@ bot.commands = {
 					description: 'Try one of the following commands for help:',
 					fields: [
 						{
-							name: 'How to get started using the bot',
-							value: `${ctk} usage`
+							name: `\`${ctk} usage\``,
+							value: 'Get started'
 						},
 						{
-							name: 'List all commands',
-							value: `${ctk} commands`
+							name: `\`${ctk} commands\``,
+							value: 'List all commands'
 						}
 					]
 				}
@@ -63,9 +87,7 @@ bot.commands = {
 		}
 	},
 	usage: {
-		token: 'usage',
-		descr: 'Help with usage of the generator',
-		example: `${ctk} usage`,
+		descr: 'Get started',
 		action: (msg) => {
 			msg.reply({
 				embed: {
@@ -75,87 +97,107 @@ bot.commands = {
 					fields: [
 						{
 							name: '1. Type the make command',
-							value: `${ctk} make`
+							value: `\`${ctk} make\``
 						},
 						{
 							name:
 								'2. Attach your sheet/table file to the message',
 							value:
-								'Supported file types: ' +
-								bot.filetypes.sheet.join(', ')
+								'Supported file types: `' +
+								bot.filetypes.sheet.join(', ') +
+								'`'
 						},
 						{
 							name:
 								'3. Attach background image ' +
 								bot.filetypes.image.join(', ') +
-								' or set background color via command',
-							value: `${ctk} make c=${cfg.colors.bg}`
+								' or append background color hexcode to command',
+							value: `\`${ctk} make c=${cfg.colors.bg}\``
 						},
 						{
 							name: '4. Optionally, also set table & font color',
-							value: `${ctk} make c=${cfg.colors.bg} t=${cfg.colors.table} f=${cfg.colors.font}`
+							value: `\`${ctk} make c=${cfg.colors.bg} t=${cfg.colors.table} f=${cfg.colors.font}\``
 						}
 					]
 				}
 			})
 		}
 	},
-	commands: {
-		token: 'commands',
+	list: {
 		descr: 'List all commands',
-		example: `${ctk} commands`,
 		action: (msg) => {
 			msg.reply({ embed: bot.helptable })
 		}
 	},
 	make: {
-		token: 'make',
 		descr: 'Make the image',
-		example: `${ctk} make`,
 		action: (msg) => {
 			let sheet = msgSheet(msg)
 			if (msg.attachments.size > 0 && sheet) {
-				let bgtype = 'customcolor'
+				let cc = msg.cleanContent
 				let bg =
-					msg.match(/c=#[0-9a-f]{6}/gi).replace('c=#', '') ||
-					(msgImage(msg) && (bgtype = 'image')) ||
-					(cfg.colors.bg && (bgtype = 'defaultcolor'))
+					cc.match(/c=#[0-9a-f]{6}/gi)?.replace('c=#', '') ||
+					msgImage(msg) ||
+					cfg.colors.bg
+				let bgtype =
+					bg == cfg.colors.bg
+						? 'defaultcolor'
+						: ['customcolor', 'image'][msgImage(msg)]
 				let tablecolor =
-					msg.match(/t=#[0-9a-f]{6}/gi).replace('t=#') ||
+					cc.match(/t=#[0-9a-f]{6}/gi)?.replace('t=#') ||
 					cfg.colors.table
 				let fontcolor =
-					msg.match(/f=#[0-9a-f]{6}/gi).replace('f=#') ||
+					cc.match(/f=#[0-9a-f]{6}/gi)?.replace('f=#') ||
 					cfg.colors.font
-				msg.reply(`Making table image with following params:\n
-							\t	Background\n
-							\t\t	type:\t${bgtype},\n
-							\t\t	bg:\t${bg}\n
-							\t	Tablecolor:\t\t${tablecolor}\n
-							\t	Fontcolor:\t\t${fontcolor}`)
+				msg.reply(
+					`Making table image with following params:
+						Background
+							type:			${bgtype},
+							bg:				${bg}
+						Tablecolor:		${tablecolor}
+						Fontcolor:		${fontcolor}`
+				)
 			} else {
 				msg.reply('No attachment found!')
 			}
+		}
+	},
+	uptime: {
+		descr: 'Check bot uptime',
+		action: (msg) => {
+			msg.reply(`Dude man I'm up since like ${msToTime(client.uptime)}.`)
+		}
+	},
+	yo: {
+		descr: 'Simple I/O testing command.',
+		action: (msg) => {
+			msg.reply(cfg.teststring)
 		}
 	}
 }
 
 bot.helptable = {
 	color: cfg.msgcolor,
-	title: `Tablebot Help: Commands`,
+	title: `Tablebot Help: Command List`,
 	description: `Here's a list of all available commands:`,
-	fields: Object.values(bot.commands).reduce((a, v) => {
-		return [
-			...a,
-			{
-				name: `\`${v.example}\``,
-				value:
-					v.descr +
-					'\n' +
-					'Options: ' +
-					(v.options ? v.options : 'None'),
-				inline: true
-			}
-		]
+	fields: Object.entries(bot.commands).reduce((a, e) => {
+		return !e[1].hidden
+			? [
+					...a,
+					{
+						name: `\`${(e[1].example
+							? e[1].example
+							: cfg.defaultexample
+						)
+							.replace(/(%t)/gi, e[0])
+							.replace(/(%ctk)/, ctk)}\``,
+						value:
+							e[1].descr +
+							(e[1].options ? '\nOptions: ' + e[1].options : ''),
+						inline: true
+					}
+			  ]
+			: a
 	}, [])
 }
 
@@ -171,16 +213,16 @@ client.on('message', async (msg) => {
 
 	// Check if command and execute if true
 	let token = msg.cleanContent.replace(new RegExp(`(.*)?${ctk}`, 'gi'), '')
-
-	let cur_command = Object.values(bot.commands).find((e) =>
-		token.includes(e.token)
+	let cur_command = Object.entries(bot.commands).find((e) =>
+		token.includes(e[0].toString())
 	)
-	if (cur_command && cur_command.action) {
-		cur_command.action(msg)
+	if (cur_command && cur_command[1].action) {
+		cur_command[1].action(msg)
 		return true
 	}
 
-	msg.reply(`You need something? try \`${ctk} ${bot.commands.help.token}\``)
+	// No matching command
+	msg.reply(`You need something? try \`${ctk} help\``)
 })
 
 // login
